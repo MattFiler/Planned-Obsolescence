b340ec0fe73c97f17b1ccf5e21641a23b798edf0
Matt Filer <matthew.filer@hotmail.co.uk>
2019-01-23 13:18:02 +0000

    Began working on map editor

---
+++ MapMaker/PO_MapMaker/TileEditor.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace PO_MapMaker
+{
+    partial class TileEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addTile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addSet = new System.Windows.Forms.Button();
+            this.tileSet = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addTile
+            // 
+            this.addTile.Location = new System.Drawing.Point(15, 169);
+            this.addTile.Name = "addTile";
+            this.addTile.Size = new System.Drawing.Size(249, 26);
+            this.addTile.TabIndex = 0;
+            this.addTile.Text = "Add Tile";
+            this.addTile.UseVisualStyleBackColor = true;
+            this.addTile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tile Sprite";
+            // 
+            // addSet
+            // 
+            this.addSet.Location = new System.Drawing.Point(429, 411);
+            this.addSet.Name = "addSet";
+            this.addSet.Size = new System.Drawing.Size(249, 43);
+            this.addSet.TabIndex = 3;
+            this.addSet.Text = "ADD SET";
+            this.addSet.UseVisualStyleBackColor = true;
+            this.addSet.Click += new System.EventHandler(this.addSet_Click);
+            // 
+            // tileSet
+            // 
+            this.tileSet.FormattingEnabled = true;
+            this.tileSet.Location = new System.Drawing.Point(15, 31);
+            this.tileSet.Name = "tileSet";
+            this.tileSet.Size = new System.Drawing.Size(249, 21);
+            this.tileSet.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tile Set";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Points of Interest";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 110);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Computer";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(92, 110);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(49, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Door";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(65, 146);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(51, 17);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "Right";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(15, 146);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(44, 17);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "Left";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Available Exits";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(122, 146);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(40, 17);
+            this.checkBox5.TabIndex = 14;
+            this.checkBox5.Text = "Up";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(168, 146);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(54, 17);
+            this.checkBox6.TabIndex = 15;
+            this.checkBox6.Text = "Down";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 20);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TileEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 634);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tileSet);
+            this.Controls.Add(this.addSet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.addTile);
+            this.Name = "TileEditor";
+            this.Text = "TileEditor";
+            this.Load += new System.EventHandler(this.TileEditor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addTile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addSet;
+        private System.Windows.Forms.ComboBox tileSet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ MapMaker/PO_MapMaker/TileEditor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+
+namespace PO_MapMaker
+{
+    public partial class TileEditor : Form
+    {
+        public TileEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void TileEditor_Load(object sender, EventArgs e)
+        {
+        }
+
+        /* Add tile to config */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load
+            XDocument configXML = XDocument.Load("config.xml");
+
+            /* NEW TILE */
+            XElement newTileParent = new XElement("tile",
+                new XAttribute("set", ""),
+                new XAttribute("sprite", ""),
+
+                new XElement("dimensions",
+                    new XAttribute("width", ""),
+                    new XAttribute("heigth", "")
+                ),
+                new XElement("valid_exits",
+                    new XAttribute("left", ""),
+                    new XAttribute("right", ""),
+                    new XAttribute("up", ""),
+                    new XAttribute("down", "")
+                ),
+                new XElement("points_of_interest",
+                    new XAttribute("computer", ""),
+                    new XAttribute("door", "")
+                )
+            );
+            configXML.Element("config").Element("tile_config").Element("tiles").Add(newTileParent);
+            
+            //Save
+            configXML.Save("config.xml");
+        }
+
+        /* Add tile set to config */
+        private void addSet_Click(object sender, EventArgs e)
+        {
+            //Load
+            XDocument configXML = XDocument.Load("config.xml");
+            
+            /* NEW SET */
+            XElement newSet = new XElement("set",
+                new XAttribute("name", "")
+            );
+            configXML.Element("config").Element("tile_config").Element("sets").Add(newSet);
+
+            //Save
+            configXML.Save("config.xml");
+        }
+    }
+}
