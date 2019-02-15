9fa47e41503b90302681ec670df81767aba3da5f
Toby Jones <tobyjones1993@gmail.com>
2019-02-15 13:54:57 +0000

    Created virtual class UI as the base for all UI elements

---
+++ Source/UI/UI.h
@@ -0,0 +1,23 @@
+#ifndef PLANNEDOBSOLESCENCE_UI_H
+#define PLANNEDOBSOLESCENCE_UI_H
+
+#include "../../Libs/ASGE/include/Engine/Renderer.h"
+#include "../Math/Point.h"
+
+class UI
+{
+ public:
+  UI() = default;
+  UI(Point pos, ASGE::Renderer* rend) : position(pos), renderer(rend){};
+  virtual ~UI() = default;
+
+  virtual void render(double delta_time) = 0;
+
+  void moveTo(Point point) { position = point; };
+
+ protected:
+  Point position;
+  ASGE::Renderer* renderer;
+};
+
+#endif // PLANNEDOBSOLESCENCE_UI_H
