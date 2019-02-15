08f9df997047da1f439b3414992308f9885f31ea
Matt Filer <matthew.filer@hotmail.co.uk>
2019-02-15 21:16:47 +0000

    checking all the values

--- Source/Managers/CharacterManager.h
+++ Source/Managers/CharacterManager.h
@@ -18,35 +18,37 @@ class CharacterManager
   template<class CharacterType>
   bool spawnCharacter(CharacterType& new_character)
   {
-    // Get our correct variables for type
-    int& character_count =
-      (IS_TYPE(Goon))
-        ? goon_count
-        : (IS_TYPE(Boss)) ? boss_count : (IS_TYPE(Security)) ? security_count : technician_count;
-    CharacterType& character_instances =
-      (IS_TYPE(Goon))
-        ? goon_instances
-        : (IS_TYPE(Boss)) ? boss_instances
-                          : (IS_TYPE(Security)) ? security_instances : technician_instances;
-
-    // Create our array if it doesn't exist
-    if (character_instances == nullptr)
+    if (IS_TYPE(Goon) || IS_TYPE(Boss) || IS_TYPE(Security) || IS_TYPE(LabTechnician))
     {
-      character_instances = new CharacterType[new_character.getSpawnCap()];
-    }
+      // Get our correct variables for type
+      int& character_count =
+        (IS_TYPE(Goon))
+          ? goon_count
+          : (IS_TYPE(Boss)) ? boss_count : (IS_TYPE(Security)) ? security_count : technician_count;
+      CharacterType& character_instances =
+        (IS_TYPE(Goon))
+          ? goon_instances
+          : (IS_TYPE(Boss)) ? boss_instances
+                            : (IS_TYPE(Security)) ? security_instances : technician_instances;
 
-    // Spawn if we haven't exceeded our limits
-    if (character_count < new_character.getSpawnCap())
-    {
-      new_character.wake(renderer);
-      new_character.setCharacterID(character_count);
-      new_character.generatePathfindingMap(game_map);
-      character_instances[character_count] = new_character;
-      character_count++;
-      return true;
-    }
+      // Create our array if it doesn't exist
+      if (character_instances == nullptr)
+      {
+        character_instances = new CharacterType[new_character.getSpawnCap()];
+      }
 
-    // We've exceeded our spawn limits...
+      // Spawn if we haven't exceeded our limits
+      if (character_count < new_character.getSpawnCap())
+      {
+        new_character.wake(renderer);
+        new_character.setCharacterID(character_count);
+        new_character.generatePathfindingMap(game_map);
+        character_instances[character_count] = new_character;
+        character_count++;
+        return true;
+      }
+    }
+    // We've exceeded our spawn limits, or have had the wrong type requested
     return false;
   }
 
