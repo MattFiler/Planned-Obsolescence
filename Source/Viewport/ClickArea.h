2e0e9ef60a314defa61a47047e1f2d9f88b558e6
Toby Jones <tobyjones1993@gmail.com>
2019-02-17 18:22:59 +0000

    Switched ClickArea to use position, width and height instead of top_left and top_right point.

--- Source/Viewport/ClickArea.h
+++ Source/Viewport/ClickArea.h
@@ -8,15 +8,19 @@ class ClickArea
  public:
   ClickArea() = default;
   /* Sets up the area for this object by using 2 points to 'draw' a box */
-  ClickArea(Point top_right_corner, Point bottom_left_corner) :
-    top_right(top_right_corner), bottom_left(bottom_left_corner){};
+  ClickArea(Point pos, float _width, float _height) :
+    position(pos), width(_width), height(_height){};
   ~ClickArea() = default;
+
+  void setHeight(float _height) { height = _height; };
+  void setWidth(float _width) { width = _width; };
 
   bool isPointInArea(Point point);
 
  private:
-  Point top_right = Point(0, 0);
-  Point bottom_left = Point(0, 0);
+  Point position;
+  float width;
+  float height;
 };
 
 #endif // PLANNEDOBSOLESCENCE_CLICKAREA_H
