42970585ac39009aea524563bcca984d182d2b9d
Toby Jones <tobyjones1993@gmail.com>
2019-02-20 14:06:25 +0000

    Added lab techs logic, needs testing once POI interaction is implemented.

--- Source/Characters/LabTechnician.h
+++ Source/Characters/LabTechnician.h
@@ -2,12 +2,28 @@
 #define PO_LABTECHIE
 
 #include "Character.h"
+#include <queue>
 
 class LabTechnician : public Character
 {
  public:
   LabTechnician() : Character(character_type::TECHNICIAN){};
-  ~LabTechnician() = default;
+  ~LabTechnician() override = default;
+
+  void lockedDoorFound() override;
+  void update(double delta_time) override;
+  void addRepairRequest(Point point) { broken_pois.push(point); };
+
+  unsigned long long int getRepairQueueLength() { return broken_pois.size(); }
+
+ private:
+  Point idle_position = Point(0, 0);
+
+  double repair_time = 10000;
+  double time_elapsed = 0;
+  bool repairing = false;
+
+  std::queue<Point> broken_pois;
 };
 
 #endif
